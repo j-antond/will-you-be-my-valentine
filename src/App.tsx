96710e712f8cc20245adf32a283bf8bf105eef45
j-antond <javieranton.dev@gmail.com>
2026-02-11 16:57:53 +0100

    Mejoras

--- src/App.tsx
+++ src/App.tsx
@@ -65,10 +65,7 @@ export default function Page() {
             <button
               onClick={handleNoClick}
               className="absolute rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700 transition-all"
-              style={{
-                top: noButtonPosition.top,
-                left: noButtonPosition.left,
-              }}
+            
             >
               {getNoButtonText()}
             </button>
